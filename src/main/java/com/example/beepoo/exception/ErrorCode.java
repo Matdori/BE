82db375338d88de99c9d8d4fcd633a78cccb016e
이지영 <plantingrass@gmail.com>
2023-07-13 13:41:59 +0900

    init: 에러 코드 핸들러 추가
    #BP-5

--- src/main/java/com/example/beepoo/exception/ErrorCode.java
+++ src/main/java/com/example/beepoo/exception/ErrorCode.java
@@ -1,14 +1,18 @@
 package com.example.beepoo.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 public enum ErrorCode {
-
-    NotFoundItem(HttpStatus.NOT_FOUND.value(), "I001", "해당 비품을 찾을 수 없습니다.");
+    // 400 BAD_REQUEST : 잘못된 요청
+    // 401 UNAUTHORIZED : 권한 없음
+    // 404 NOT_FOUND : 존재하지 않는 자원
+    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "I001", "해당 비품을 찾을 수 없습니다."),
+    // 409 CONFLICT : 자원 충돌, 중복 데이터
+    ITEM_EXIST(HttpStatus.CONFLICT.value(), "I002", "이미 존재하는 비품입니다.");
 
     private final int httpStatus;
     private final String errorCode;
