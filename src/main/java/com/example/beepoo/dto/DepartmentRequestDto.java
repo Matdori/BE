39bf8b468b74c5b4a56d26aeebe7ac43aa35cb5d
Woozy <106052832+anfrosus@users.noreply.github.com>
2023-07-13 11:29:43 +0900

    Revert "feat:부서등록기능 구현"
    This reverts commit 4dcab82a5cbcc039292fecb3d91e3917ffba7bcc.

--- src/main/java/com/example/beepoo/dto/DepartmentRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.beepoo.dto;
-
-import lombok.Getter;
-
-@Getter
-public class DepartmentRequestDto {
-    //부서명
-    private String departmentName;
-}
--- src/main/java/com/example/beepoo/dto/DepartmentResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.beepoo.dto;
-
-import lombok.Getter;
-
-@Getter
-public class DepartmentResponseDto {
-    private String a;
-}
