c344526e0973fb59990fd72b00245baff8dcb74a
이지영 <plantingrass@gmail.com>
2023-07-14 12:04:27 +0900

    feat: 비품 목록 조회 기능 개발
    #BP-7

--- src/main/java/com/example/beepoo/dto/ItemDto.java
+++ src/main/java/com/example/beepoo/dto/ItemDto.java
@@ -1,42 +1,49 @@
 package com.example.beepoo.dto;
 
 import com.example.beepoo.entity.ItemEntity;
-import lombok.*;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class ItemDto {
     private int seq;
-
     private String name;
-
-    private int itemTypeCode;
-
-    private int itemStatusCode;
-
+    private int typeCode;
+    private int statusCode;
     private String serial;
-
     private String comment;
-
     private LocalDateTime createDate;
-
-    private LocalDateTime modifyDate;
-
     private String createUser;
-
+    private LocalDateTime modifyDate;
     private String modifyUser;
 
     public ItemDto(ItemEntity entity){
         this.seq = entity.getSeq();
         this.name = entity.getName();
-        this.itemTypeCode = entity.getItemTypeCode();
-        this.itemStatusCode = entity.getItemStatusCode();
+        this.typeCode = entity.getTypeCode();
+        this.statusCode = entity.getStatusCode();
         this.serial = entity.getSerial();
         this.comment = entity.getComment();
         this.createDate = entity.getCreateDate();
-        this.modifyDate = entity.getCreateDate();
         this.createUser = entity.getCreateUser();
+        this.modifyDate = entity.getModifyDate();
         this.modifyUser = entity.getModifyUser();
     }
+
+    @QueryProjection
+    public ItemDto(int seq, String name, int typeCode, int statusCode, String serial, String comment, LocalDateTime createDate, String createUser, LocalDateTime modifyDate, String modifyUser) {
+        this.seq = seq;
+        this.name = name;
+        this.typeCode = typeCode;
+        this.statusCode = statusCode;
+        this.serial = serial;
+        this.comment = comment;
+        this.createDate = createDate;
+        this.createUser = createUser;
+        this.modifyDate = modifyDate;
+        this.modifyUser = modifyUser;
+    }
 }
+
