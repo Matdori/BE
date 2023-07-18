5360e1f7f8a208168752cfda1d5232765b2a32d4
Woozy <106052832+anfrosus@users.noreply.github.com>
2023-07-18 12:08:08 +0900

    feat: validation 기능 구현
    #BP-13

--- src/main/java/com/example/beepoo/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/beepoo/exception/CustomExceptionHandler.java
@@ -1,13 +1,22 @@
 package com.example.beepoo.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
         return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<List<ErrorResponseEntity>> handleValidationException(MethodArgumentNotValidException e) {
+        return ErrorResponseEntity.toResponseEntityValid(e);
     }
 }
--- src/main/java/com/example/beepoo/exception/ErrorResponseEntity.java
+++ src/main/java/com/example/beepoo/exception/ErrorResponseEntity.java
@@ -1,14 +1,23 @@
 package com.example.beepoo.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
 public class ErrorResponseEntity {
     private int status;
     private String code;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String field;
     private String message;
 
     public static ResponseEntity<ErrorResponseEntity> toResponseEntity(ErrorCode e){
@@ -20,5 +29,26 @@ public class ErrorResponseEntity {
                         .message(e.getMessage())
                         .build()
                 );
+    }
+
+    //검증 시
+    public static ResponseEntity<List<ErrorResponseEntity>> toResponseEntityValid(MethodArgumentNotValidException e){
+
+        List<ErrorResponseEntity> errorResponseEntityList = new ArrayList<>();
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+
+            errorResponseEntityList.add(
+                    ErrorResponseEntity.builder()
+                            .status(HttpStatus.BAD_REQUEST.value())
+                            .code(fieldError.getCode())
+                            .field(fieldError.getField())
+                            .message(fieldError.getDefaultMessage())
+                            .build()
+            );
+        }
+        return ResponseEntity
+                .status(e.getStatusCode().value())
+                .body(errorResponseEntityList);
     }
 }
