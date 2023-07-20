592148e44ffb81d90228ce6542ab8d52bd8649f4
이지영 <plantingrass@gmail.com>
2023-07-20 19:07:32 +0900

    feat: 초기 admin 계정 추가
    #BP-16

--- src/main/java/com/example/beepoo/repository/UserRepository.java
+++ src/main/java/com/example/beepoo/repository/UserRepository.java
@@ -1,16 +1,16 @@
 package com.example.beepoo.repository;
 
 import com.example.beepoo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUserName(String userName);
+
     boolean existsByUserEmail(String userEmail);
 
     Optional<User> findUserByUserEmail(String userEmail);
 
     List<User> findUsersByDepartmentName(String departmentName);
-
 }
