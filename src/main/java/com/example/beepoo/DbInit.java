e2ca357e73b14b18a77e66d8a1e7a0a663a53012
이지영 <plantingrass@gmail.com>
2023-07-20 19:07:32 +0900

    feat: 초기 admin 계정 추가
    #BP-16

---
+++ src/main/java/com/example/beepoo/DbInit.java
@@ -0,0 +1,38 @@
+package com.example.beepoo;
+
+import com.example.beepoo.entity.User;
+import com.example.beepoo.enums.UserRoleEnum;
+import com.example.beepoo.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DbInit {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    private void init() {
+        // 초기 admin 계정 추가
+        if (!userRepository.existsByUserName("admin")) {
+            String encodedPassword = passwordEncoder.encode("beepoo123");
+
+            User user = new User();
+            user.setUserName("admin");
+            user.setUserEmail("admin");
+            user.setUserPassword(encodedPassword);
+            user.setPosition("admin");
+            user.setUserRole(UserRoleEnum.ADMIN);
+            user.setCreateUser("SYSTEM");
+
+            userRepository.save(user);
+        }
+    }
+}
