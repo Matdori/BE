e5f6ad8c4e79e5e867de66890b1955f3f6f19ca4
이지영 <plantingrass@gmail.com>
2023-07-20 12:14:41 +0900

    feat: 요청 목록 조회 시, 비품명/요청자/처리자 검색 옵션 추가
    #BP-11

--- src/main/java/com/example/beepoo/dto/AskRequestDto.java
+++ src/main/java/com/example/beepoo/dto/AskRequestDto.java
@@ -7,16 +7,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AskRequestDto {
-    private Integer seq;
-    private Integer itemSeq;
-    private AskTypeEnum askType;
-    private Long askUserId;
-    private String askMsg;
-    private String askImg;
-    private Long confirmUserId;
-    private String confirmMsg;
-    private String createDate;
-    private String createUser;
-    private String modifyDate;
-    private String modifyUser;
+
+  private Integer seq;
+  private Integer itemSeq;
+  private AskTypeEnum askType;
+  private Long askUserId;
+  private String askMsg;
+  private String askImg;
+  private Long confirmUserId;
+  private String confirmMsg;
+  private String createDate;
+  private String createUser;
+  private String modifyDate;
+  private String modifyUser;
+  // 비품명 검색 key
+  private String itemName;
+  // 요청자 검색 key
+  private String askUserName;
+  // 처리자 검색 key
+  private String confirmUserName;
 }
--- src/main/java/com/example/beepoo/repository/AskCustomRepositoryImpl.java
+++ src/main/java/com/example/beepoo/repository/AskCustomRepositoryImpl.java
@@ -1,8 +1,14 @@
 package com.example.beepoo.repository;
 
+import static com.example.beepoo.entity.QAsk.ask;
+import static com.example.beepoo.entity.QItem.item;
+import static com.example.beepoo.entity.QUser.user;
+import static org.springframework.util.StringUtils.hasText;
+
 import com.example.beepoo.dto.AskRequestDto;
 import com.example.beepoo.dto.AskResponseDto;
 import com.example.beepoo.dto.QAskResponseDto;
+import com.example.beepoo.entity.QUser;
 import com.example.beepoo.enums.AskTypeEnum;
 import com.example.beepoo.util.OrderByNull;
 import com.querydsl.core.types.Order;
@@ -10,20 +16,16 @@ import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import static com.example.beepoo.entity.QAsk.ask;
-import static com.example.beepoo.entity.QUser.user;
-import static org.springframework.util.StringUtils.hasText;
-
 @Repository
 public class AskCustomRepositoryImpl implements AskCustomRepository {
+
     private final JPAQueryFactory jpaQueryFactory;
 
     public AskCustomRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
@@ -32,49 +34,70 @@ public class AskCustomRepositoryImpl implements AskCustomRepository {
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+    // 요청자, 처리자 User 테이블을 구분하기 위한 confirmUser
+    QUser confirmUser = new QUser("confirmUser");
+
     @Override
     public List<AskResponseDto> getAskList(AskRequestDto condition, Pageable pageable) {
         List<AskResponseDto> askList = jpaQueryFactory
-                .select(new QAskResponseDto(
-                        ask.seq,
-                        ask.item,
-                        ask.type,
-                        ask.askUser,
-                        ask.askMsg,
-                        ask.askImg,
-                        ask.confirmUser,
-                        ask.confirmMsg,
-                        ask.createDate,
-                        ask.createUser,
-                        ask.modifyDate,
-                        ask.modifyUser))
-                .from(ask)
-                .leftJoin(ask.confirmUser, user)
-                .where(
-                        typeEq(condition.getAskType()),
-                        createDateRangeEq(condition.getCreateDate()),
-                        createUserEq(condition.getCreateUser()),
-                        modifyDateRangeEq(condition.getModifyDate()),
-                        modifyUserEq(condition.getModifyUser())
+            .select(
+                new QAskResponseDto(
+                    ask.seq,
+                    ask.item,
+                    ask.type,
+                    ask.askUser,
+                    ask.askMsg,
+                    ask.askImg,
+                    ask.confirmUser,
+                    ask.confirmMsg,
+                    ask.createDate,
+                    ask.createUser,
+                    ask.modifyDate,
+                    ask.modifyUser
                 )
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .orderBy(OrderBy(pageable))
-                .fetch();
+            )
+            .from(ask)
+            .join(ask.askUser, user)
+            .leftJoin(ask.confirmUser, confirmUser)
+            .where(
+                itemNameEq(condition.getItemName()),
+                askUserNameEq(condition.getAskUserName()),
+                confirmUserNameEq(condition.getConfirmUserName()),
+                typeEq(condition.getAskType()),
+                createDateRangeEq(condition.getCreateDate()),
+                createUserEq(condition.getCreateUser()),
+                modifyDateRangeEq(condition.getModifyDate()),
+                modifyUserEq(condition.getModifyUser())
+            )
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy(OrderBy(pageable))
+            .fetch();
 
         return askList;
     }
 
-    // TODO(337): item 테이블 name 으로 비품명으로 조회
-    // TODO(337): user 테이블 name 으로 요청자 조회
-    // TODO(337): user 테이블 name 으로 처리자 조회
+    // item 테이블 name 으로 비품명으로 조회
+    private BooleanExpression itemNameEq(String itemName) {
+        return hasText(itemName) ? item.name.contains(itemName) : null;
+    }
+
+    // user 테이블 name 으로 요청자 조회
+    private BooleanExpression askUserNameEq(String askUserName) {
+        return hasText(askUserName) ? user.userName.contains(askUserName) : null;
+    }
+
+    // user 테이블 name 으로 처리자 조회
+    private BooleanExpression confirmUserNameEq(String confirmUserName) {
+        return hasText(confirmUserName) ? confirmUser.userName.contains(confirmUserName) : null;
+    }
 
     private BooleanExpression typeEq(AskTypeEnum type) {
         return type != null ? ask.type.in(type) : null;
     }
 
     private BooleanExpression createDateRangeEq(String createDateRange) {
-        if(hasText(createDateRange)){
+        if (hasText(createDateRange)) {
             String start = createDateRange.split("~")[0] + " 00:00:00";
             String end = createDateRange.split("~")[1] + " 23:59:59";
 
@@ -82,7 +105,7 @@ public class AskCustomRepositoryImpl implements AskCustomRepository {
             LocalDateTime endDate = LocalDateTime.parse(end, formatter);
 
             return ask.createDate.between(startDate, endDate);
-        }else{
+        } else {
             return null;
         }
     }
@@ -92,7 +115,7 @@ public class AskCustomRepositoryImpl implements AskCustomRepository {
     }
 
     private BooleanExpression modifyDateRangeEq(String modifyDateRange) {
-        if(hasText(modifyDateRange)){
+        if (hasText(modifyDateRange)) {
             String start = modifyDateRange.split("~")[0] + " 00:00:00";
             String end = modifyDateRange.split("~")[1] + " 23:59:59";
 
@@ -100,7 +123,7 @@ public class AskCustomRepositoryImpl implements AskCustomRepository {
             LocalDateTime endDate = LocalDateTime.parse(end, formatter);
 
             return ask.modifyDate.between(startDate, endDate);
-        }else{
+        } else {
             return null;
         }
     }
@@ -114,7 +137,7 @@ public class AskCustomRepositoryImpl implements AskCustomRepository {
         Sort sort = pageable.getSort();
 
         // 정렬 옵션이 없는 경우
-        if(Sort.unsorted().equals(sort)){
+        if (Sort.unsorted().equals(sort)) {
             return result;
         }
 
