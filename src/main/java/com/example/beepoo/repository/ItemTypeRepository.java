a8565cbb212690e85bef68566719f551f5011cb7
이지영 <plantingrass@gmail.com>
2023-07-28 11:16:02 +0900

    feat: 비품 조회 시, 비품 종류 표시
    #BP-7

--- src/main/java/com/example/beepoo/repository/ItemTypeRepository.java
+++ src/main/java/com/example/beepoo/repository/ItemTypeRepository.java
@@ -3,9 +3,6 @@ package com.example.beepoo.repository;
 import com.example.beepoo.entity.ItemType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ItemTypeRepository extends JpaRepository<ItemType, Long> {
     boolean existsItemTypeByType(String type);
-
 }
