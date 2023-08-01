9c342a9dc497d8a6407b31996e7706c32e221e5c
이지영 <plantingrass@gmail.com>
2023-08-01 23:24:02 +0900

    feat: 비품 목록 조회, 요청 현황 조회 totalCnt 추가
    #BP-7
    #BP-11

--- src/main/java/com/example/beepoo/repository/ItemCustomRepository.java
+++ src/main/java/com/example/beepoo/repository/ItemCustomRepository.java
@@ -1,13 +1,12 @@
 package com.example.beepoo.repository;
 
 import com.example.beepoo.dto.ItemRequestDto;
-import com.example.beepoo.dto.ItemResponseDto;
+import java.util.List;
+import java.util.Map;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface ItemCustomRepository {
-    List<ItemResponseDto> getItemList(ItemRequestDto condition, Pageable pageable);
+    Map<String, Object> getItemList(ItemRequestDto condition, Pageable pageable);
 
     void updateItem(ItemRequestDto itemDto);
 
